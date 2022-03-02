4f041b9f6cb87ddc830227787de2ff337f6cb9a2
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2022-03-02 10:31:06 +0530

    added main() by Dev 1

--- a.java
+++ a.java
@@ -3,5 +3,9 @@ class Test{
 public static void main(String[] args) {
     System.out.println("added by TL");
 }
+
+public static void main() {
+    System.out.println("added by Dev 1");
+}
      
 }
