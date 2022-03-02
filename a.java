4e3a65b4d08e8c469be71674bb974e5058ce3850
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2022-03-02 10:34:36 +0530

    main(String args) added by TL

--- a.java
+++ a.java
@@ -3,5 +3,8 @@ class Test{
 public static void main(String[] args) {
     System.out.println("added by TL");
 }
+public static void main(String args) {
+    System.out.println("added by TL");
+}
      
 }
